6693b9eadbb2f52addfe9228af66a72557c4f42f
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-11-14 07:57:36 -0600

    Update ad_1.js

--- ad_1.js
+++ ad_1.js
@@ -1,7 +1,2 @@
 /// ad_filter.js
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 192) {
-    let code = prompt()
-    eval(code);
-  }
-});
+document.title = "67 " + document.title
